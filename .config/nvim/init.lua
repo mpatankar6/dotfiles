1d130e792667922879308b6980e216357d4d1eb0
Mihir Patankar <mpatankar06@gmail.com>
2025-11-22 21:04:49 -0500

    add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,5 @@
+--theme
+vim.cmd.colorscheme("unokai")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", {bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
