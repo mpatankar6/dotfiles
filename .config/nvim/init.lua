af8c684685cf1c755ec3f9ca14f207fc282f353c
Mihir Patankar <mpatankar06@gmail.com>
2025-12-02 12:25:06 -0500

    replace lazy.nvim with native nvim.pack

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,33 +1,34 @@
 -- 1. Global settings
 vim.g.mapleader = " "
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 2
+vim.o.cursorline = true
+vim.o.cursorlineopt = "number"
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
-
--- 2. Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({
-    "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath,
-  })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
--- 3. Setup lazy.nvim
-require("lazy").setup({
-  spec = { { import = "plugins" } },
-  checker = { enabled = true },
+-- Plugins
+vim.pack.add({
+				"https://github.com/catppuccin/nvim.git"
 })
 
+-- Plugin Configuration
+require("catppuccin").setup({
+				flavour = "mocha",
+				transparent_background = true,
+				auto_integrations = true,
+})
+vim.cmd("colorscheme catppuccin")
+
 -- 4. Keymaps and Autocmds
+
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save current buffer" })
+vim.keymap.set("n", "<leader>W", ":wa<CR>", { desc = "Save all buffers" })
+
+local telescope = require("telescope.builtin")
+
+
+vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "List Buffers" })
+vim.keymap.set("n", "<leader>fh", telescope.help_tags, { desc = "Help Tags" })
+vim.keymap.set("n", "<leader>fgf", telescope.git_files, { desc = "Git Files" })
