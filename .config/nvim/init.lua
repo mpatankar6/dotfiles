a83fbba74a30bb984b02d68b2344bc0193820079
Mihir Patankar <mpatankar06@gmail.com>
2025-12-03 21:22:41 -0500

    attempt a minimal clean nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,34 +1,86 @@
--- 1. Global settings
+-- Global settings and options
+vim.diagnostic.config({ virtual_text = true })
 vim.g.mapleader = " "
+
+vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.shiftwidth = 2
 vim.o.tabstop = 2
-vim.o.cursorline = true
-vim.o.cursorlineopt = "number"
+vim.o.expandtab = true
+vim.o.signcolumn = "yes"
+vim.o.winborder = "rounded"
 
 -- Plugins
 vim.pack.add({
-				"https://github.com/catppuccin/nvim.git"
+  "https://github.com/catppuccin/nvim.git",
+  "https://github.com/ibhagwan/fzf-lua.git",
+  "https://github.com/neovim/nvim-lspconfig.git",
+  "https://github.com/nvim-lualine/lualine.nvim.git",
+  "https://github.com/nvim-treesitter/nvim-treesitter.git",
+  "https://github.com/stevearc/oil.nvim.git",
+  { src = "https://github.com/saghen/blink.cmp.git", version = vim.version.range("*") },
 })
 
--- Plugin Configuration
 require("catppuccin").setup({
-				flavour = "mocha",
-				transparent_background = true,
-				auto_integrations = true,
+  flavour = "mocha",
+  transparent_background = true,
+  auto_integrations = true,
 })
 vim.cmd("colorscheme catppuccin")
+require('fzf-lua').setup()
+require('lualine').setup({})
+---@diagnostic disable-next-line: missing-fields
+require('nvim-treesitter.configs').setup({
+  auto_install = true,
+  highlight = { enable = true },
+  additional_vim_regex_highlighting = false
+})
+require('oil').setup()
+require('blink.cmp').setup({
+  signature = { enabled = true },
+  completion = {
+    documentation = { auto_show = true, auto_show_delay_ms = 500 },
+    ghost_text = { enabled = true },
+  },
+  keymap = { preset = "enter" }
+})
 
--- 4. Keymaps and Autocmds
+-- LSP configuration
+vim.lsp.enable({ "lua_ls" })
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      workspace = { library = vim.api.nvim_get_runtime_file("", true) }
+    }
+  }
+})
 
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save current buffer" })
-vim.keymap.set("n", "<leader>W", ":wa<CR>", { desc = "Save all buffers" })
+-- Autocmds
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    if ev.data.spec.name == "nvim-treesitter/nvim-treesitter" and
+        (ev.data.kind == "install" or ev.data.kind == "update") then
+      vim.cmd.packadd("nvim-treesitter")
+      vim.cmd("TSUpdate")
+    end
+  end,
+})
 
-local telescope = require("telescope.builtin")
+-- Keymaps
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save Current Buffer" })
+vim.keymap.set("n", "<leader>W", ":wa<CR>", { desc = "Save All Buffers" })
+vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y', { desc = 'Copy to System Clipboard' })
+vim.keymap.set('n', '<leader>p', '"+p', { desc = 'Paste from System Clipboard' })
+vim.keymap.set('x', '<leader>p', '"+P', { desc = 'Paste from System Clipboard' })
+vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format buffer" })
+vim.keymap.set("n", "<leader>O", ":Oil<CR>", { desc = "Open Oil" })
 
-
-vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Find Files" })
-vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Live Grep" })
-vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "List Buffers" })
-vim.keymap.set("n", "<leader>fh", telescope.help_tags, { desc = "Help Tags" })
-vim.keymap.set("n", "<leader>fgf", telescope.git_files, { desc = "Git Files" })
+local fzf_lua = require("fzf-lua")
+vim.keymap.set("n", "<leader>ff", fzf_lua.files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fr", fzf_lua.registers, { desc = "Find Registers" })
+vim.keymap.set("n", "<leader>fg", fzf_lua.live_grep, { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>fm", fzf_lua.marks, { desc = "Find Marks" })
+vim.keymap.set("n", "<leader>fb", fzf_lua.buffers, { desc = "Find Buffers" })
+vim.keymap.set("n", "<leader>fh", fzf_lua.helptags, { desc = "Find Help Tags" })
+vim.keymap.set("n", "<leader>s", fzf_lua.spell_suggest, { desc = "Spelling Suggestions" })
