5e37ef116c2f198099ba566e784d13008126ea8c
Mihir Patankar <mpatankar06@gmail.com>
2026-02-15 15:32:11 -0500

    add override module for ghostty on linux

---
+++ modules/ghostty-extra-config.nix
@@ -0,0 +1,6 @@
+# On Linux I'd like to change some ghostty options.
+# For example, text scaling works differently, so I need a smaller font size.
+
+{
+  home.file.".config/ghostty/linux.config".text = "font-size = 12";
+}
