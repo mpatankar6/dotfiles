f724ac2c0264cf9936965f40d47d3c3a31e41fa6
Mihir Patankar <mpatankar06@gmail.com>
2026-02-17 23:31:31 -0500

    add mako module

---
+++ modules/desktop/mako.nix
@@ -0,0 +1,12 @@
+{
+  services.mako = {
+    enable = true;
+    settings = {
+      font = "JetBrainsMono Nerd Font 11";
+      border-radius = 8;
+      padding = "10";
+      default-timeout = 5000;
+    };
+  };
+  catppuccin.mako.enable = true;
+}
