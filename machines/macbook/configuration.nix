8e2764ff0e97144ef16353d95e292558e5dfeaea
Mihir Patankar <mpatankar06@gmail.com>
2026-02-18 21:11:09 -0500

    update macbook configuration

--- machines/macbook/configuration.nix
+++ machines/macbook/configuration.nix
@@ -4,18 +4,6 @@
     "nix-command"
     "flakes"
   ];
-  environment.systemPackages = with pkgs; [
-    utm
-  ];
-  homebrew = {
-    enable = true;
-    casks = [
-      "ghostty"
-      "crossover"
-    ];
-    onActivation.upgrade = true;
-    onActivation.cleanup = "zap";
-  };
   programs.fish.enable = true;
   users.users.mihir = {
     name = "mihir";
@@ -28,9 +16,23 @@
     NSGlobalDomain.KeyRepeat = 2;
     dock.autohide = true;
     dock.showAppExposeGestureEnabled = true;
+    dock.persistent-apps = [
+      "/System/Cryptexes/App/System/Applications/Safari.app/"
+      "/System/Applications/Messages.app/"
+      "/System/Applications/Mail.app/"
+      "/System/Applications/Notes.app/"
+      "/System/Applications/System Settings.app/"
+      "/Users/mihir/Applications/Home Manager Apps/Alacritty.app/"
+    ];
     finder.AppleShowAllExtensions = true;
     finder.ShowPathbar = true;
     finder.ShowStatusBar = true;
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+    CustomUserPreferences = {
+      "com.apple.Safari" = {
+        AlwaysRestoreSessionAtLaunch = true;
+      };
+    };
   };
   system.stateVersion = 6;
 }
