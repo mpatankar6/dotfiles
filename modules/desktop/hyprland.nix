2595fc9eb39abcba1ef0a6432a9004e118789b88
Mihir Patankar <mpatankar06@gmail.com>
2026-02-18 20:26:45 -0500

    add hyprland module

---
+++ modules/desktop/hyprland.nix
@@ -0,0 +1,91 @@
+{ lib, ... }:
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      general = {
+        "col.active_border" = "rgba($mauveAlphaee) rgba($blueAlphaee) 45deg";
+        "col.inactive_border" = "rgba($surface0Alphaaa)";
+      };
+      dwindle = {
+        preserve_split = true;
+      };
+      decoration = {
+        rounding = 10;
+        inactive_opacity = 0.9;
+        blur.size = 4;
+        shadow.color = "rgba($crustAlphaee)";
+      };
+      animations = {
+        bezier = [
+          "easeOutQuint, 0.23, 1, 0.32, 1"
+          "easeInOutCubic, 0.65, 0.05, 0.36, 1"
+          "linear, 0, 0, 1, 1"
+          "almostLinear, 0.5, 0.5, 0.75, 1"
+          "quick, 0.15, 0, 0.1, 1"
+        ];
+        animation = [
+          "global, 1, 10, default"
+          "border, 1, 5.39, easeOutQuint"
+          "windows, 1, 4.79, easeOutQuint"
+          "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+          "windowsOut, 1, 1.49, linear, popin 87%"
+          "fadeIn, 1, 1.73, almostLinear"
+          "fadeOut, 1, 1.46, almostLinear"
+          "fade, 1, 3.03, quick"
+          "layers, 1, 3.81, easeOutQuint"
+          "layersIn, 1, 4, easeOutQuint, fade"
+          "layersOut, 1, 1.5, linear, fade"
+          "fadeLayersIn, 1, 1.79, almostLinear"
+          "fadeLayersOut, 1, 1.39, almostLinear"
+          "workspaces, 1, 1.94, almostLinear, fade"
+          "workspacesIn, 1, 1.21, almostLinear, fade"
+          "workspacesOut, 1, 1.94, almostLinear, fade"
+          "zoomFactor, 1, 7, quick"
+        ];
+      };
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, f, fullscreen, 0"
+        "$mod SHIFT, f, togglefloating"
+        "$mod, q, killactive"
+        "$mod, h, movefocus, l"
+        "$mod, l, movefocus, r"
+        "$mod, k, movefocus, u"
+        "$mod, j, movefocus, d"
+        "$mod, s, togglespecialworkspace, scratch"
+        "$mod SHIFT, s, movetoworkspace, special:scratch"
+        "$mod, tab, workspace, previous"
+        "$mod, space, exec, rofi -show drun"
+        "$mod, return, exec, ghostty"
+      ]
+      ++ builtins.concatLists (
+        map (ws: [
+          "$mod, ${ws}, workspace, ${ws}"
+          "$mod SHIFT, ${ws}, movetoworkspace, ${ws}"
+        ]) (map toString (lib.range 1 9))
+      );
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ];
+      bindl = [
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
+      ];
+      windowrule = [
+        "suppress_event maximize, match:class .*"
+        "float on, size 600 400, center on, match:class \\.blueman.*"
+      ];
+    };
+    systemd.variables = [ "--all" ];
+  };
+  catppuccin.hyprland.enable = true;
+}
