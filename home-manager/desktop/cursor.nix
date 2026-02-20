45991609efe7924c811de704ad2dc7029f1a32da
Mihir Patankar <mpatankar06@gmail.com>
2026-02-19 22:05:15 -0500

    fix cursor in some apps

--- home-manager/desktop/cursor.nix
+++ home-manager/desktop/cursor.nix
@@ -4,8 +4,17 @@
   home.pointerCursor = {
     package = pkgs.apple-cursor;
     name = "macOS";
+    size = 24;
     x11.enable = true;
     gtk.enable = true;
     hyprcursor.enable = true;
   };
+
+  # This causes apps like Steam to inherit the correct settings
+  home.file.".icons/default/index.theme".text = ''
+    [Icon Theme]
+    Name=Default
+    Comment=Default Cursor Theme
+    Inherits=macOS
+  '';
 }
