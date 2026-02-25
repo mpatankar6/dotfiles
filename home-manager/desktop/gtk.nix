3153c0eff30d13f169df79c525cd890b5c6c2c9f
Mihir Patankar <mpatankar06@gmail.com>
2026-02-25 15:51:03 -0500

    simplify gtk module

--- home-manager/desktop/gtk.nix
+++ home-manager/desktop/gtk.nix
@@ -7,15 +7,8 @@
       name = "Adwaita-dark";
       package = pkgs.gnome-themes-extra;
     };
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
   };
+  catppuccin.gtk.icon.enable = true;
 }
