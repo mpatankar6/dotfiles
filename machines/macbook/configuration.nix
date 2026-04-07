3331f098988ac071911b3c8d5321c5e2f6f4f08c
Mihir Patankar <mpatankar06@gmail.com>
2026-04-07 18:50:24 -0400

    add keyboard mappings

--- machines/macbook/configuration.nix
+++ machines/macbook/configuration.nix
@@ -10,32 +10,38 @@
     home = "/Users/mihir";
     shell = pkgs.fish;
   };
-  system.primaryUser = "mihir";
-  system.defaults = {
-    NSGlobalDomain.InitialKeyRepeat = 17;
-    NSGlobalDomain.KeyRepeat = 2;
-    dock.showAppExposeGestureEnabled = true;
-    dock.persistent-apps = [
-      "/System/Cryptexes/App/System/Applications/Safari.app/"
-      "/System/Applications/Messages.app/"
-      "/System/Applications/Calendar.app/"
-      "/System/Applications/Mail.app/"
-      "/System/Applications/Notes.app/"
-      "/System/Applications/System Settings.app/"
-      "/Users/mihir/Applications/Home Manager Apps/Ghostty.app/"
-    ];
-    dock.persistent-others = [
-      "/Users/mihir/Downloads"
-    ];
-    finder.AppleShowAllExtensions = true;
-    finder.ShowPathbar = true;
-    finder.ShowStatusBar = true;
-    NSGlobalDomain.AppleInterfaceStyle = "Dark";
-    CustomUserPreferences = {
-      "com.apple.Safari" = {
-        AlwaysRestoreSessionAtLaunch = true;
+  system = {
+    primaryUser = "mihir";
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
+    defaults = {
+      NSGlobalDomain.InitialKeyRepeat = 25;
+      NSGlobalDomain.KeyRepeat = 6;
+      dock.showAppExposeGestureEnabled = true;
+      dock.persistent-apps = [
+        "/System/Cryptexes/App/System/Applications/Safari.app/"
+        "/System/Applications/Messages.app/"
+        "/System/Applications/Calendar.app/"
+        "/System/Applications/Mail.app/"
+        "/System/Applications/Notes.app/"
+        "/System/Applications/System Settings.app/"
+        "/Users/mihir/Applications/Home Manager Apps/Ghostty.app/"
+      ];
+      dock.persistent-others = [
+        "/Users/mihir/Downloads"
+      ];
+      finder.AppleShowAllExtensions = true;
+      finder.ShowPathbar = true;
+      finder.ShowStatusBar = true;
+      NSGlobalDomain.AppleInterfaceStyle = "Dark";
+      CustomUserPreferences = {
+        "com.apple.Safari" = {
+          AlwaysRestoreSessionAtLaunch = true;
+        };
       };
     };
+    stateVersion = 6;
   };
-  system.stateVersion = 6;
 }
