af277ad178f84265640fe039a8ff63637bfd57be
Mihir Patankar <mpatankar06@gmail.com>
2026-04-07 18:22:15 -0400

    increase ghostty font size on mac

--- home-manager/shareable/ghostty.nix
+++ home-manager/shareable/ghostty.nix
@@ -9,7 +9,8 @@
       window-padding-y = 10;
       background-opacity = 0.8;
       background-blur = true;
-      font-size = 14;
+      # MacOS renders fonts smaller.
+      font-size = if pkgs.stdenv.isDarwin then 16 else 14;
       shell-integration-features = "ssh-terminfo";
       keybind = "ctrl+alt+o=toggle_background_opacity";
       # On Linux, this lets ghostty be a persistent daemon.
