0411f18bc9dd880d92d891fb0e10fd3c6ae3c47e
Mihir Patankar <mpatankar06@gmail.com>
2026-04-11 21:59:28 -0400

    make weather script try and wait for internet before failing

--- home-manager/desktop/waybar/weather.sh
+++ home-manager/desktop/waybar/weather.sh
@@ -7,6 +7,10 @@ on_error() {
 trap on_error ERR
 set -o pipefail
 
+for i in $(seq 5); do
+  ping -c1 -W2 1.1.1.1 &>/dev/null && break || sleep 2
+done
+
 location_data=$(curl -s 'https://ipinfo.io/json')
 coords=$(echo "$location_data" \
   | jq -r '.loc | split(",") | "latitude=\(.[0])&longitude=\(.[1])"')
