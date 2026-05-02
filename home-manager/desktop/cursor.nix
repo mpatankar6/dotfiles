6ff19cf48d68910610c9727da783940b1ed15be4
Mihir Patankar <mpatankar06@gmail.com>
2026-05-02 19:48:20 -0400

    change cursor to bibata ice

--- home-manager/desktop/cursor.nix
+++ home-manager/desktop/cursor.nix
@@ -2,8 +2,8 @@
 
 {
   home.pointerCursor = {
-    package = pkgs.apple-cursor;
-    name = "macOS";
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
     size = 24;
     x11.enable = true;
     gtk.enable = true;
